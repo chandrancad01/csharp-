dc68d6f85813e9e6af6efef3f2ec4b1fc4b01e10
chandrancad01 <chandrancad01@gmail.com>
2024-02-21 16:44:14 +0530

    added files

---
+++ session2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+class Progra{
+
+  static void Main(string [] args){
+    Console.WriteLine("Welcome To Classes");
+    test1 tt=new test1();
+    tt.PrintMSG();
+    tt.PrintMSG2();
+
+  }
+}
+
+
+public class test1{
+  string name="CAD BATCH 01";
+
+  public void PrintMSG(){
+    Console.WriteLine(name);
+  }
+
+  public void PrintMSG2(){
+    Console.WriteLine("hi ..! " + name);
+  }
+
+}
