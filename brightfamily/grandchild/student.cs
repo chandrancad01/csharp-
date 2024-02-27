489881d4ac7174ba74ba343df64c86eb8aa7ab32
chandrancad01 <chandrancad01@gmail.com>
2024-02-27 15:11:33 +0530

    some files added and some files modified

---
+++ brightfamily/grandchild/student.cs
@@ -0,0 +1,13 @@
+﻿
+namespace brightfamily{
+
+    class student : bright {
+          
+        public student(){
+            Console.WriteLine(" I am Gopal in Student off cad batch");
+            Console.WriteLine(" Cad batch student using some tools c++, c");
+            Console.WriteLine("Student time is 2:00pm to 6:00pm");
+        }
+
+    }
+}
---
+++ brightfamily/parent/bright.cs
@@ -0,0 +1,30 @@
+namespace brightfamily{
+       class bright{
+         string Branchmanager,Branchstaff,Student, course;
+           public bright(){
+            Branchmanager="Ferosh";
+            Branchstaff="Karthick";
+            Student="Gopal";
+            course="Ai , Cad";
+
+}
+            public string getBranchmanager(){
+                return Branchmanager;
+            }
+
+             public string getBranchstaff(){
+                return Branchstaff;
+            }
+
+                public string getStudent(){
+                                return Student;
+            }
+                            public string getcourses(){
+                                return course;
+            }
+
+ }
+}
+     
+         
+    
